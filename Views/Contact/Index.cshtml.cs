d2b75a7e7b99e5e7d3b90ecd33da83ab34d1fbf7
diGiusepp3 <matthi_gielen@hotmail.be>
2025-09-17 20:33:18 +0200

    Added database setup

---
+++ Views/Contact/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿// FILE: Views/Pages/Contact/Contact.cshtml.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webcrafters.be_ASP.NET_Core_project.Pages.Contact
+{
+    public class ContactModel : PageModel
+    {
+        [BindProperty] public string Name { get; set; }
+        [BindProperty] public string Email { get; set; }
+        [BindProperty] public string Message { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            // Hier kan je e-mail versturen of DB-entry maken
+            TempData["Success"] = $"Bericht van {Name} is verzonden!";
+            return RedirectToPage("/Contact/Contact");
+        }
+    }
+}
